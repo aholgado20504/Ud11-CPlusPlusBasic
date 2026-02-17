602a3506a53369e3cb7991f86e90ef8f71a0f259
aholgado20504 <aholgado20504@iesjoanramis.org>
2026-02-17 10:29:53 +0100

    Exercici 4

---
+++ c++/exercici4.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <math.h>
+#include <string>
+using namespace std;
+
+int main() {
+    string a, b;
+    cout << "Escriu el teu nom: ";
+    cin>>a;
+    cout << "Escriu el teu primer cognom: ";
+    cin>>b;
+    cout<<"Hola, "<<a<<" "<<b;
+    return 0;
+}
